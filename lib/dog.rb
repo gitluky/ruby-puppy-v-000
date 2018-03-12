ba401667f2fd106ea4facddfb341ad064e45987f
Jonathan Luk <jluk87@gmail.com>
2018-03-12 20:15:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @all << self
+
+  end
+
+  def self.clear_all
+    @all.clear
+  end
+
+  def self.all
+    @@all
+  end
 
 end
